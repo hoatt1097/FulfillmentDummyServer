695581a90190ea45adb0074f6b023230040ec821
QuyenTNT <thuyquyen2905@gmail.com>
2022-11-11 09:26:35 +0700

    Update endpoint

--- FulfillmentDummyServer/Controllers/HomeController.cs
+++ FulfillmentDummyServer/Controllers/HomeController.cs
@@ -13,7 +13,7 @@ namespace FulfillmentDummyServer.Controllers
     public class HomeController : ControllerBase
     {
         [HttpGet]
-        [Route("orders/getOrders")]
+        [Route("vendor/directFulfillment/orders/v1/purchaseOrders")]
         public IActionResult GetOrders()
         {
             var reader = new StreamReader("FakeData/GetOrders.json");
@@ -23,7 +23,7 @@ namespace FulfillmentDummyServer.Controllers
         }
 
         [HttpGet]
-        [Route("orders/getOrders/{purchaseOrderNumber}")]
+        [Route("vendor/directFulfillment/orders/v1/purchaseOrders/{purchaseOrderNumber}")]
         public IActionResult GetOrder()
         {
             var reader = new StreamReader("FakeData/GetOrder.json");
@@ -33,7 +33,7 @@ namespace FulfillmentDummyServer.Controllers
         }
 
         [HttpPost]
-        [Route("orders/acknowledgements")]
+        [Route("vendor/directFulfillment/orders/v1/acknowledgements")]
         public IActionResult SubmitAcknowledgements(Object body)
         {
             var reader = new StreamReader("FakeData/TransactionId.json");
@@ -43,7 +43,7 @@ namespace FulfillmentDummyServer.Controllers
         }
 
         [HttpPost]
-        [Route("shipping/shippingLabels")]
+        [Route("vendor/directFulfillment/shipping/v1/shippingLabels")]
         public IActionResult SubmitShippingLabels(Object body)
         {
             var reader = new StreamReader("FakeData/TransactionId.json");
@@ -53,7 +53,7 @@ namespace FulfillmentDummyServer.Controllers
         }
 
         [HttpGet]
-        [Route("shipping/shippingLabels")]
+        [Route("vendor/directFulfillment/shipping/v1/shippingLabels")]
         public IActionResult ShippingLabels()
         {
             var reader = new StreamReader("FakeData/ShippingLabels.json");
@@ -63,7 +63,7 @@ namespace FulfillmentDummyServer.Controllers
         }
 
         [HttpGet]
-        [Route("shipping/shippingLabels/{purchaseOrderNumber}")]
+        [Route("vendor/directFulfillment/shipping/v1/shippingLabels/{purchaseOrderNumber}")]
         public IActionResult ShippingLabel()
         {
             var reader = new StreamReader("FakeData/ShippingLabel.json");
@@ -73,7 +73,7 @@ namespace FulfillmentDummyServer.Controllers
         }
 
         [HttpPost]
-        [Route("shipping/shipmentConfirmations")]
+        [Route("vendor/directFulfillment/shipping/v1/shipmentConfirmations")]
         public IActionResult SubmitShipmentConfirmations(Object body)
         {
             var reader = new StreamReader("FakeData/TransactionId.json");
@@ -83,7 +83,7 @@ namespace FulfillmentDummyServer.Controllers
         }
 
         [HttpPost]
-        [Route("shipping/shipmentStatusUpdates")]
+        [Route("vendor/directFulfillment/shipping/v1/shipmentStatusUpdates")]
         public IActionResult SubmitShipmentStatusUpdates(Object body)
         {
             var reader = new StreamReader("FakeData/TransactionId.json");
@@ -93,7 +93,7 @@ namespace FulfillmentDummyServer.Controllers
         }
 
         [HttpGet]
-        [Route("shipping/packingSlips")]
+        [Route("vendor/directFulfillment/shipping/v1/packingSlips")]
         public IActionResult PackingSlips()
         {
             var reader = new StreamReader("FakeData/PackingSlips.json");
@@ -103,7 +103,7 @@ namespace FulfillmentDummyServer.Controllers
         }
 
         [HttpGet]
-        [Route("shipping/packingSlips/{purchaseOrderNumber}")]
+        [Route("vendor/directFulfillment/shipping/v1/packingSlips/{purchaseOrderNumber}")]
         public IActionResult PackingSlip()
         {
             var reader = new StreamReader("FakeData/PackingSlip.json");
@@ -113,7 +113,7 @@ namespace FulfillmentDummyServer.Controllers
         }
 
         [HttpGet]
-        [Route("shipping/customerInvoices")]
+        [Route("vendor/directFulfillment/shipping/v1/customerInvoices")]
         public IActionResult CustomerInvoices()
         {
             var reader = new StreamReader("FakeData/CustomerInvoices.json");
@@ -123,7 +123,7 @@ namespace FulfillmentDummyServer.Controllers
         }
 
         [HttpGet]
-        [Route("shipping/customerInvoices/{purchaseOrderNumber}")]
+        [Route("vendor/directFulfillment/shipping/v1/customerInvoices/{purchaseOrderNumber}")]
         public IActionResult CustomerInvoice()
         {
             var reader = new StreamReader("FakeData/CustomerInvoice.json");
@@ -133,7 +133,7 @@ namespace FulfillmentDummyServer.Controllers
         }
 
         [HttpPost]
-        [Route("inventory/warehouses/{warehouseId}/items")]
+        [Route("vendor/directFulfillment/inventory/v1/warehouses/{warehouseId}/items")]
         public IActionResult SubmitInventoryUpdate(Object body)
         {
             var reader = new StreamReader("FakeData/TransactionId.json");
@@ -143,7 +143,7 @@ namespace FulfillmentDummyServer.Controllers
         }
 
         [HttpPost]
-        [Route("payments/invoices")]
+        [Route("vendor/directFulfillment/payments/v1/invoices")]
         public IActionResult SubmitInvoices(Object body)
         {
             var reader = new StreamReader("FakeData/TransactionId.json");
